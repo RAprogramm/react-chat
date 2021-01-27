0ddffbf3bc6cd3e6a880e828b5755371466f4c1b
RAprogramm <andrey.rozanov.vl@gmail.com>
2021-01-27 17:12:09 +0900

    messages server side

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -9,6 +9,8 @@ let socket
 const Chat = ({ location }) => {
   const [name, setName] = useState('')
   const [room, setRoom] = useState('')
+  const [message, setMessage] = useState('')
+  const [messages, setMessages] = useState([])
   const ENDPOINT = 'localhost:5000'
 
   useEffect(() => {
@@ -21,8 +23,19 @@ const Chat = ({ location }) => {
     setName(name)
     setRoom(room)
 
-    socket.emit('join', { name, room })
+    socket.emit('join', { name, room }, () => {})
+    return () => {
+      socket.emit('disconnect')
+      
+      socket.off()
+    }
   }, [ENDPOINT, location.search])
+  
+  useEffect(() => {
+    socket.on('message', (message) => {
+      setMessages([...messages, message])
+    })
+  }, [messages])
 
   return (
     <div className='outerContainer'>
